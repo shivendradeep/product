f05557912b1f1b04a62b1c819abf2c8060a985ae
shivendradeep <69365525+shivendradeep@users.noreply.github.com>
2022-08-01 15:47:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http');
 
 app.get("/",function(req,resp){
   //resp.sendFile("public/empdata.html",{root:__dirname});
-  //resp.sendFile("/product.html",{root:__dirname});
+  resp.sendFile("/product.html",{root:__dirname});
 })
 app.listen(9000, '0.0.0.0', () => {
     console.log('server started on port 9000')
