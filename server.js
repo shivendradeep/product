084515585e38e5674d2853a0d7dd5034fe55c37b
shivendradeep <shivendradeep939@gmail.com>
2022-08-01 15:20:05 +0530

    added

---
+++ server.js
@@ -0,0 +1,12 @@
+const { response } = require('express');
+var express=require('express');
+var app=express();
+var http = require('http');
+
+app.get("/",function(req,resp){
+  //resp.sendFile("public/empdata.html",{root:__dirname});
+  //resp.sendFile("/product.html",{root:__dirname});
+})
+app.listen(9000, '0.0.0.0', () => {
+    console.log('server started on port 9000')
+  })
